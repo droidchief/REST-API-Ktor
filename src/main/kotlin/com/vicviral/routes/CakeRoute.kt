f1c64179a4d18e71eed8b2111f101955a19164a3
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-09-21 18:42:18 +0100

    Fix: delete serialization routing

--- src/main/kotlin/com/vicviral/routes/CakeRoute.kt
+++ src/main/kotlin/com/vicviral/routes/CakeRoute.kt
@@ -8,7 +8,7 @@ import io.ktor.routing.*
 
 private const val BASE_URL =  "https://192.168.1.144:8080"
 
-private val cakes = listOf<Cake>(
+private val cakes = listOf(
     Cake(1, "dotch cake", "The best thing you will ever taste", "Butter Cake", "$BASE_URL/cakes/cake1.jpeg"),
     Cake(2, "frad cake", "The best thing you will ever taste", "Ice Cake", "$BASE_URL/cakes/cake2.jpeg"),
     Cake(3, "german cake", "The best thing you will ever taste", "Fondant Cake", "$BASE_URL/cakes/cake3.jpeg"),
